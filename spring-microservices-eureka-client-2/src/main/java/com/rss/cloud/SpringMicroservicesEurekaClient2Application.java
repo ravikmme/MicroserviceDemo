f6a4e22cae47ce337314bb7beedefa9c81fef871
ravikmme <44118820+ravikmme@users.noreply.github.com>
2019-01-17 02:32:24 +0530

    Microservice via spring boot

---
+++ spring-microservices-eureka-client-2/src/main/java/com/rss/cloud/SpringMicroservicesEurekaClient2Application.java
@@ -0,0 +1,30 @@
+package com.rss.cloud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class SpringMicroservicesEurekaClient2Application {
+
+	@Autowired
+	private EurekaClient client;
+	
+	@RequestMapping("/serviceinfo")
+	public String serviceInfo() {
+		InstanceInfo instance = client.getNextServerFromEureka("myOtherClient", false);
+		return instance.getHomePageUrl();
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicroservicesEurekaClient2Application.class, args);
+	}
+}
